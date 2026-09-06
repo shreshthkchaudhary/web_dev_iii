585529c1b3aed2f6addadf23a4ba26c8db0db639
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-09-07 00:14:16 +0530

    adding content

--- self/02_node/require-exports/data.js
+++ self/02_node/require-exports/data.js
@@ -1,5 +1,6 @@
 let a = "Shreshth";
 let b = "Kumar";
-
+let c = "Chaudhary"
 module.exports = {a,b}
+module.exports.data_c = {c}
 // console.log(module)
--- self/02_node/require-exports/main.js
+++ self/02_node/require-exports/main.js
@@ -5,4 +5,5 @@ let function_01= require("./function")
 
 function_01(data.a)
 function_01(data.b)
+function_01(data.data_c.c)
 // console.log(module)
